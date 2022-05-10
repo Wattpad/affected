4ea4058a00670fa35f5d9d49ea13fe82ec64ffc3
Chris Dueck <chris.dueck@wattpad.com>
2022-05-10 09:38:11 -0400

    Remove unnecessary intermediate data
    The `modifiedPackages` var is created only to later select the
    `ImportPath` field of each `Package`. Since the import paths are already
    stored in the `pkgSeen` map, it's an unnecessary intermediate.

--- main.go
+++ main.go
@@ -37,7 +37,6 @@ func main() {
 	repo := gitRoot()
 
 	pkgSeen := make(map[string]bool)
-	var modifiedPackages []*build.Package
 	buildContext := build.Default
 	for _, f := range files {
 		if isIgnored(f) {
@@ -59,7 +58,6 @@ func main() {
 		}
 
 		pkgSeen[pkg.ImportPath] = true
-		modifiedPackages = append(modifiedPackages, pkg)
 	}
 
 	// TODO: list all packages in the repo, determine dependency tree and filter package list to those that transitively import affected packages
@@ -100,8 +98,8 @@ func main() {
 
 	// filter the package list to those affected
 	var affectedPackages []string
-	for _, p := range modifiedPackages {
-		affectedPackages = append(affectedPackages, p.ImportPath)
+	for path := range pkgSeen {
+		affectedPackages = append(affectedPackages, path)
 	}
 	addedMore := true
 	for addedMore {
